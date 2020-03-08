e66ea1fd8b23ce9edf40c595819dc9d79b09e64d
jansona <alelipona121@gmail.com>
2020-03-09 02:07:01 +0800

    finish smtp login interface

--- Rust_DLL_Test_Stage/Rust_DLL_Test_Stage/TestPlace.cs
+++ Rust_DLL_Test_Stage/Rust_DLL_Test_Stage/TestPlace.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace Rust_DLL_Test_Stage
 {
     class TestPlace
     {
+        [DllImport("smtplib.dll", EntryPoint = "validate_account")]
+        private static extern Boolean validate_account_smtp(string account, string passwd, string site);
 
         public static void Main(string[] args)
         {
-            
+            if(validate_account_smtp("ale_li_pona@163.com","ybg19970203ybg","smtp.163.com:25"))
+            {
+                Console.WriteLine("Succ");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+
+            Console.ReadKey();
         }
+
     }
 
 }
--- stmplib/src/lib.rs
+++ stmplib/src/lib.rs
@@ -1,7 +1,133 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::net::TcpStream;
+use std::io::{Read, Write};
+use std::str;
+use base64::encode;
+use chrono::prelude::*;
+use libc::c_char;
+use std::ffi::{CString, CStr};
+
+static CRLF: &str = "\r\n";
+
+struct Mail {
+    from: String,
+    to: String,
+    cc: String,
+    subject: String,
+    body: String
+}
+
+impl Mail {
+    pub fn new(from: String, to: String, cc: String, subject: String, body: String) -> Mail {
+        Mail {
+            from: from,
+            to: to,
+            cc: cc,
+            subject: subject,
+            body: body
+        }
     }
 }
+
+fn get_response(socket: &mut TcpStream) -> String{
+    let mut buf: [u8; 1024] = [0; 1024];
+
+    let _ = socket.read(&mut buf);
+    let response = str::from_utf8(&buf).unwrap();
+    String::from(response)
+}
+
+fn judge_response(response: &str) -> bool {
+    
+    let result = match &response[..1] {
+        "2" => true,
+        "3" => true,
+        _ => false
+    };
+
+    result
+}
+
+fn write_request(socket: &mut TcpStream, message: &str) {
+    let message = format!("{}{}", message, "\r\n");
+    let _ = socket.write(&(message.as_bytes()));
+}
+
+fn write_request_b64(socket: &mut TcpStream, message: &str) {
+    let message = encode(message.as_bytes());
+    println!("{}", message);
+    let _ = socket.write(&(message.as_bytes()));
+    let _ = socket.write("\r\n".as_bytes());
+}
+
+fn send_head(socket: &mut TcpStream, mail: &Mail) {
+    let now = Utc::now();
+
+    write_request(socket, &format!("Date: {}", now));
+    write_request(socket, &format!("From: a {}", mail.from));
+    write_request(socket, &format!("To: b {}", mail.to));
+    write_request(socket, &format!("Cc: c {}", mail.cc));
+    write_request(socket, &format!("Subject: {}", mail.subject));
+}
+
+fn send_body(socket: &mut TcpStream, mail: &Mail) {
+    let _ = socket.write(mail.body.as_bytes());
+    let _ = socket.write(format!("{}.{}", CRLF, CRLF).as_bytes());
+}
+
+fn send_mail(socket: &mut TcpStream, mail: Mail) {
+    write_request(socket, &format!("MAIL FROM: <{}>", mail.from));
+    get_response(socket);
+
+    write_request(socket, &format!("RCPT TO: <{}>", mail.to));
+    get_response(socket);
+
+    write_request(socket, "DATA");
+    get_response(socket);
+
+    send_head(socket, &mail);
+    // get_response(socket);
+
+    write_request(socket, "");
+
+    send_body(socket, &mail);
+    // get_response(socket);
+}
+
+fn unwrap_str(s: *const c_char) -> String {
+    let s = unsafe { CStr::from_ptr(s) };
+    String::from(s.to_str().unwrap())
+}
+
+#[no_mangle]
+pub extern "C" fn validate_account(account: *const c_char, passwd: *const c_char, site: *const c_char) -> bool {
+
+    let account_str = unwrap_str(account);
+    let passwd_str = unwrap_str(passwd);
+    let site_str = unwrap_str(site);
+
+    let mut responses: Vec<String> = Vec::new();
+    let tokens:Vec<&str>= account_str.split("@").collect();
+
+    let mut stream = TcpStream::connect(&site_str).unwrap();
+    responses.push(get_response(&mut stream));
+
+    write_request(&mut stream, &format!("HELO {}", tokens[0]));
+    responses.push(get_response(&mut stream));
+
+    write_request(&mut stream, "AUTH LOGIN");
+    responses.push(get_response(&mut stream));
+
+    write_request_b64(&mut stream, &account_str);
+    responses.push(get_response(&mut stream));
+
+    write_request_b64(&mut stream, &passwd_str);
+    responses.push(get_response(&mut stream));
+
+    for response in responses {
+        if !judge_response(&response) {
+            return false;
+        }
+    }
+    true
+}
+
