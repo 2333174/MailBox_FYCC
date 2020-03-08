dc6c1c81a07d81ce7ae847480c37a86b2af32bcf
jansona <alelipona121@gmail.com>
2020-03-08 16:26:12 +0800

    set up env for developing DLL

---
+++ Rust_DLL_Test_Stage/Rust_DLL_Test_Stage/TestPlace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rust_DLL_Test_Stage
+{
+    class TestPlace
+    {
+
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+
+}
