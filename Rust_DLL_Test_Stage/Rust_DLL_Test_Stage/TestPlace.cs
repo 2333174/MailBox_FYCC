bc67123bdc3dd6852834384a2836fe571754ccbe
jansona <alelipona121@gmail.com>
2020-03-09 14:56:37 +0800

    finish pop3 login interface

--- Rust_DLL_Test_Stage/Rust_DLL_Test_Stage/TestPlace.cs
+++ Rust_DLL_Test_Stage/Rust_DLL_Test_Stage/TestPlace.cs
@@ -13,12 +13,27 @@ namespace Rust_DLL_Test_Stage
 {
     class TestPlace
     {
+        #region SMTP/POP3 身份验证
+        // smtp 身份验证
         [DllImport("smtplib.dll", EntryPoint = "validate_account")]
         private static extern Boolean validate_account_smtp(string account, string passwd, string site);
 
-        public static void Main(string[] args)
+        // pop3 身份验证
+        [DllImport("pop3lib.dll", EntryPoint = "validate_account")]
+        private static extern Boolean validate_account_pop3(string account, string passwd, string site);
+
+        static void Validate_Example()
         {
-            if(validate_account_smtp("alertdoll@163.com","ybgiscute","smtp.163.com:25"))
+            if (validate_account_smtp("alertdoll@163.com", "ybgissocute2020", "smtp.163.com:25"))
+            {
+                Console.WriteLine("Succ");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+
+            if (validate_account_pop3("alertdoll@163.com", "ybgissocute2020", "pop.163.com:110"))
             {
                 Console.WriteLine("Succ");
             }
@@ -28,6 +43,12 @@ namespace Rust_DLL_Test_Stage
             }
 
             Console.ReadKey();
+        }
+        #endregion
+
+        public static void Main(string[] args)
+        {
+            Validate_Example();
         }
 
     }
