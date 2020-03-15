838c8f1189e0e771c01eb6a702d60cf24b74e6c7
jansona <alelipona121@gmail.com>
2020-03-15 23:13:22 +0800

    WTF :smile:

--- Rust_DLL_Test_Stage/Rust_DLL_Test_Stage/TestPlace.cs
+++ Rust_DLL_Test_Stage/Rust_DLL_Test_Stage/TestPlace.cs
@@ -11,7 +11,8 @@ using System.Runtime.InteropServices;
 
 namespace Rust_DLL_Test_Stage
 {
-    class TestPlace
+    #region DEPENDENCE
+    public class MailUtil
     {
         [StructLayout(LayoutKind.Sequential)]
         public struct LoginInfo
@@ -31,21 +32,99 @@ namespace Rust_DLL_Test_Stage
             public string body;
         }
 
-        #region SMTP/POP3 身份验证
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ResultStruct
+        {
+            public Int32 status;
+            public IntPtr mail_string;
+        }
+
         // smtp 身份验证
         [DllImport("smtplib.dll", EntryPoint = "validate_account")]
-        private static extern Boolean validate_account_smtp(LoginInfo info);
+        public static extern Boolean validate_account_smtp(LoginInfo info);
 
         // pop3 身份验证
         [DllImport("pop3lib.dll", EntryPoint = "validate_account")]
-        private static extern Boolean validate_account_pop3(LoginInfo info);
+        public static extern Boolean validate_account_pop3(LoginInfo info);
+
+        [DllImport("smtplib.dll", EntryPoint = "login_send_mail")]
+        public static extern Int32 login_send_mail(LoginInfo info, MailInfo email);
+
+        [DllImport("pop3lib.dll", EntryPoint = "pull_a_mail")]
+        public static extern StringHandle pull_a_mail(LoginInfo info, UInt32 index);
+
+        [DllImport("pop3lib.dll", EntryPoint = "free_string")]
+        public static extern void free_string(IntPtr str_ptr);
+
+        [DllImport("pop3lib.dll", EntryPoint = "get_simple_email")]
+        public static extern ResultStruct get_simple_email(LoginInfo info, UInt32 index);
+    }
+    public class MailStr : IDisposable
+    {
+        private StringHandle mailStrHandle;
+        private string mailString;
+
+        public MailStr(MailUtil.LoginInfo login_info, UInt32 index)
+        {
+            mailStrHandle = MailUtil.pull_a_mail(login_info, index);
+        }
+
+        public override string ToString()
+        {
+            if (mailString == null)
+            {
+                mailString = mailStrHandle.AsString();
+            }
+            return mailString;
+        }
+
+        public void Dispose()
+        {
+            mailStrHandle.Dispose();
+        }
+    }
+
+    public class StringHandle : SafeHandle
+    {
+        public StringHandle() : base(IntPtr.Zero, true) { }
+
+        public override bool IsInvalid
+        {
+            get { return this.handle == IntPtr.Zero; }
+        }
+
+        public string AsString()
+        {
+            int len = 0;
+            while (Marshal.ReadByte(handle, len) != 0) { ++len; }
+            byte[] buffer = new byte[len];
+            Marshal.Copy(handle, buffer, 0, buffer.Length);
+            return Encoding.UTF8.GetString(buffer);
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            if (!this.IsInvalid)
+            {
+                MailUtil.free_string(handle);
+            }
+
+            return true;
+        }
+    }
+    #endregion
+
+    class TestPlace
+    {
+        #region SMTP/POP3 身份验证
 
         static void Validate_Example()
         {
-            LoginInfo info_smtp = new LoginInfo() { account = "alertdoll@163.com",
+            MailUtil.LoginInfo info_smtp 
+                = new MailUtil.LoginInfo() { account = "alertdoll@163.com",
                 passwd = "ybgissocute2020",
                 site = "smtp.163.com:25" };
-            if (validate_account_smtp(info_smtp))
+            if (MailUtil.validate_account_smtp(info_smtp))
             {
                 Console.WriteLine("Succ");
             }
@@ -54,13 +133,13 @@ namespace Rust_DLL_Test_Stage
                 Console.WriteLine("Fail");
             }
 
-            LoginInfo info_pop3 = new LoginInfo()
+            MailUtil.LoginInfo info_pop3 = new MailUtil.LoginInfo()
             {
                 account = "alertdoll@163.com",
                 passwd = "ybgissocute2020",
                 site = "pop.163.com:110"
             };
-            if (validate_account_pop3(info_pop3))
+            if (MailUtil.validate_account_pop3(info_pop3))
             {
                 Console.WriteLine("Succ");
             }
@@ -74,18 +153,16 @@ namespace Rust_DLL_Test_Stage
         #endregion
 
         #region SMTP send mail
-        [DllImport("smtplib.dll", EntryPoint = "login_send_mail")]
-        private static extern Int32 login_send_mail(LoginInfo info, MailInfo email);
         static void SendMail_Example()
         {
-            LoginInfo info_smtp = new LoginInfo()
+            MailUtil.LoginInfo info_smtp = new MailUtil.LoginInfo()
             {
                 account = "alertdoll@163.com",
                 passwd = "ybgissocute2020",
                 site = "smtp.163.com:25"
             };
 
-            MailInfo mail_info = new MailInfo()
+            MailUtil.MailInfo mail_info = new MailUtil.MailInfo()
             {
                 from="alertdoll@163.com",
                 to="ale_li_pona@163.com",
@@ -94,7 +171,7 @@ namespace Rust_DLL_Test_Stage
                 body="Haha",
             };
 
-            Int32 result = login_send_mail(info_smtp, mail_info);
+            Int32 result = MailUtil.login_send_mail(info_smtp, mail_info);
 
             Console.WriteLine(result);
 
@@ -102,11 +179,35 @@ namespace Rust_DLL_Test_Stage
         }
         #endregion
 
+        #region POP3 receive mail
+
+        static void ReceiveMail_Example()
+        {
+            MailUtil.LoginInfo info_pop3 = new MailUtil.LoginInfo()
+            {
+                account = "alertdoll@163.com",
+                passwd = "ybgissocute2020",
+                site = "pop.163.com:110"
+            };
+
+            //MailStr mail_str = new MailStr(info_pop3, 3);
+
+            //Console.WriteLine(mail_str);
+            MailUtil.ResultStruct rs = MailUtil.get_simple_email(info_pop3, 3);
+
+            Console.WriteLine(Marshal.PtrToStringAnsi(rs.mail_string));
+
+            Console.ReadKey();
+        }
+        #endregion
+
         public static void Main(string[] args)
         {
-            //Validate_Example();
+            Validate_Example();
 
             SendMail_Example();
+
+            //ReceiveMail_Example();
         }
 
     }
--- pop3lib/src/lib.rs
+++ pop3lib/src/lib.rs
@@ -1,27 +1,28 @@
 use std::net::TcpStream;
-use libc::c_char;
-use crate::utils::{unwrap_str, LoginInfo};
+use crate::utils::mail_utils::{unwrap_str, LoginInfo};
 use crate::utils::basic_utils::*;
 use crate::utils::mail_utils::*;
+use std::ffi::{CStr, CString};
+use libc::c_char;
 
-
+static mut STRING_POINTER: *mut c_char = 0 as *mut c_char;
 
 mod utils {
-    use libc::c_char;
-    use std::ffi::CStr;
     
     pub mod basic_utils {
         use regex::Regex;
         use std::net::TcpStream;
         use std::str;
         use std::io::{Read, Write};
+        use std::ffi::{CStr, CString};
+        use std::os::raw::c_char;
         
         pub fn get_response(socket: &mut TcpStream) -> String{
             let mut buf: [u8; 1024] = [0; 1024];
             
             let _ = socket.read(&mut buf);
             let response = str::from_utf8(&buf).unwrap();
-            println!("{}", response);
+            // println!("{}", response);
             String::from(response)
         }
         
@@ -54,9 +55,23 @@ mod utils {
     pub mod mail_utils {
         use std::net::TcpStream;
         use std::str;
+        use libc::c_char;
+        use std::ffi::CStr;
         use crate::utils::basic_utils::*;
-
+        
         static CRLF: &str = "\r\n";
+        
+        #[repr(C)]
+        pub struct LoginInfo {    
+            pub account: *const c_char,
+            pub passwd: *const c_char,
+            pub site: *const c_char,
+        }
+        
+        pub fn unwrap_str(s: *const c_char) -> String {
+            let s = unsafe { CStr::from_ptr(s) };
+            String::from(s.to_str().unwrap())
+        }
         
         pub fn get_multiresponses(socket: &mut TcpStream) -> String {
             let mut resp = String::new();
@@ -74,60 +89,125 @@ mod utils {
             resp
         }
         
-        pub fn authenticate(socket: &mut TcpStream, user: &str, pwd: &str) {
-            write_request(socket, &format!("{} {}", "USER", user));
-            get_response(socket);
-            write_request(socket, &format!("{} {}", "PASS", pwd));
-            // let resp = get_response(socket);
-            // let tokens:Vec<&str> = resp.split(" ").collect();
-            // tokens[1].parse().unwrap()
+        pub fn authenticate(login_info: LoginInfo) -> (bool, TcpStream) {
+            
+            let account_str = unwrap_str(login_info.account);
+            let passwd_str = unwrap_str(login_info.passwd);
+            let site_str = unwrap_str(login_info.site);
+            
+            let mut responses: Vec<String> = Vec::new();
+            
+            let mut stream = TcpStream::connect(&site_str).unwrap();
+            responses.push(get_response(&mut stream));
+            
+            write_request(&mut stream, &format!("{} {}", "USER", account_str));
+            responses.push(get_response(&mut stream));
+            write_request(&mut stream, &format!("{} {}", "PASS", passwd_str));
+            responses.push(get_response(&mut stream));
+            
+            for response in responses {
+                if !judge_response(&response) {
+                    return (false, stream);
+                }
+            }
+            (true, stream)
         }
         
-        pub fn list_mails(socket: &mut TcpStream) {
+        pub fn list_mails(socket: &mut TcpStream) -> String {
             write_request(socket, "LIST");
             
-            println!("{}", get_multiresponses(socket)); 
+            // println!("{}", get_multiresponses(socket));
+            get_multiresponses(socket) 
         }
         
         pub fn get_a_mail(socket: &mut TcpStream, index: usize) -> String {
+            println!("RETR {}", index);
             write_request(socket, &format!("RETR {}", index));
             get_multiresponses(socket)
         }
         
     }
-    
-    pub fn unwrap_str(s: *const c_char) -> String {
-        let s = unsafe { CStr::from_ptr(s) };
-        String::from(s.to_str().unwrap())
-    }
-    
-    #[repr(C)]
-    pub struct LoginInfo {    
-        pub account: *const c_char,
-        pub passwd: *const c_char,
-        pub site: *const c_char,
-    }
 }
 
 #[no_mangle]
 pub extern "C" fn validate_account(login_info: LoginInfo) -> bool {
+    authenticate(login_info).0
+}
+
+#[no_mangle]
+pub extern "C" fn pull_a_mail(login_info: LoginInfo, index: usize) -> *mut c_char {
     
-    let account_str = unwrap_str(login_info.account);
-    let passwd_str = unwrap_str(login_info.passwd);
-    let site_str = unwrap_str(login_info.site);
+    let (result, mut stream) = authenticate(login_info);
     
-    let mut responses: Vec<String> = Vec::new();
-    
-    let mut stream = TcpStream::connect(&site_str).unwrap();
-    responses.push(get_response(&mut stream));
-    
-    authenticate(&mut stream, &account_str, &passwd_str);
-    responses.push(get_response(&mut stream));
-    
-    for response in responses {
-        if !judge_response(&response) {
-            return false;
-        }
+    if !result {
+        return CString::new("400").unwrap().into_raw();
     }
-    true
+    
+    let mail_str = get_a_mail(&mut stream, index);
+    
+    let mail_cstr = CString::new(mail_str).unwrap();
+    
+    mail_cstr.into_raw()
+}
+
+
+#[repr(C)]
+pub struct ResultStruct {    
+    pub status: i32,
+    pub mail_string: *mut c_char,
+}
+
+fn store_string_on_heap(string_to_store: &String) -> *mut c_char {
+    //create a new raw pointer
+    let pntr = CString::new(String::from(string_to_store)).unwrap().into_raw();
+    //store it in our static variable (REQUIRES UNSAFE)
+    unsafe {
+        STRING_POINTER = pntr;
+    }
+    //return the c_char
+    return pntr;
+}
+
+// static mut MAIL_STR: &'static str = "" as &'static str;
+static mut MY_STRING: String = String::new();
+
+#[no_mangle]
+pub extern fn get_simple_email(login_info: LoginInfo, index: usize) -> ResultStruct {
+    
+    let (result, mut stream) = authenticate(login_info);
+    
+    unsafe {
+
+        MY_STRING = String::from("WTF");
+
+        ResultStruct {
+            status: 400,
+            mail_string: store_string_on_heap(&MY_STRING),
+        }
+
+        // if result {
+        //     MY_STRING.clear();
+        //     MY_STRING.push_str(&get_a_mail(&mut stream, index));
+            
+        //     ResultStruct {
+        //         status: 200,
+        //         mail_string: store_string_on_heap(&MY_STRING),
+        //     }
+        // } else {
+        //     MY_STRING.clear();
+            
+        //     ResultStruct {
+        //         status: 400,
+        //         mail_string: store_string_on_heap(&MY_STRING),
+        //     }
+        // }
+    }
+}
+
+#[no_mangle]
+pub extern fn free_string() {
+    unsafe {
+        let _ = CString::from_raw(STRING_POINTER);
+        STRING_POINTER = 0 as *mut c_char;
+    }
 }
