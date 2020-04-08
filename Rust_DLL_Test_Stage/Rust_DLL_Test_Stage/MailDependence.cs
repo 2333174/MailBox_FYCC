ce31499056cee24ce181bd29f8a19cc3c0327110
jansona <alelipona121@gmail.com>
2020-04-08 15:23:08 +0800

    inverse auth-logic in pop3

--- Rust_DLL_Test_Stage/Rust_DLL_Test_Stage/MailDependence.cs
+++ Rust_DLL_Test_Stage/Rust_DLL_Test_Stage/MailDependence.cs
@@ -57,6 +57,9 @@ namespace Rust_DLL_Test_Stage
 
         [DllImport("pop3lib.dll", EntryPoint = "del_mail")]
         public static extern Int32 del_mail(LoginInfo info, UInt32 index);
+
+        [DllImport("pop3lib.dll", EntryPoint = "pull_uid_mail")]
+        public static extern Int32 pull_uid_mail(LoginInfo info, UInt32 index);
     }
     #endregion
 }
